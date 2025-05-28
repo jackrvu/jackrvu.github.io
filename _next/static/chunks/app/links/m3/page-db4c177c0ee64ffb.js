b2aeb21234d9512e61535b42e2579dbb04f6c086
jackrvu <140197178+jackrvu@users.noreply.github.com>
2025-05-28 00:39:02 -0500

    deploy: update favicon and latest site changes

---
+++ _next/static/chunks/app/links/m3/page-db4c177c0ee64ffb.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[314,387,679,711,230,931,991,512,181,589],{707:function(n,e,u){Promise.resolve().then(u.t.bind(u,231,23))}},function(n){n.O(0,[231,971,23,744],function(){return n(n.s=707)}),_N_E=n.O()}]);
---
+++ _next/static/chunks/app/links/mavhub/page-5ce1fc57e93a501b.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[711,387,679,230,314,931,991,512,181,589],{707:function(n,e,u){Promise.resolve().then(u.t.bind(u,231,23))}},function(n){n.O(0,[231,971,23,744],function(){return n(n.s=707)}),_N_E=n.O()}]);
