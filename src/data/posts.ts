9378aecfd2f00096db4d52beb1883be10a94d830
jackrvu <140197178+jackrvu@users.noreply.github.com>
2025-06-26 13:02:32 -0500

    update site

--- src/data/posts.ts
+++ src/data/posts.ts
@@ -1,9 +1,9 @@
 // src/data/posts.ts
 export const posts = [
-    { date: '06-16-25', title: 'crossword puzzles, an ai benchmark for them, and thinking', slug: 'crosswordbench' },
-    { date: '06-15-25', title: 'thoughts on memory', slug: 'recall' },
-    { date: '06-15-25', title: 'workout schedule', slug: 'workout' },
-    { date: '06-12-25', title: 'the new new york socialist', slug: 'zohran' },
+    //{ date: '06-16-25', title: 'crossword puzzles, an ai benchmark for them, and thinking', slug: 'crosswordbench' },
+    ///{ date: '06-15-25', title: 'thoughts on memory', slug: 'recall' },
+    //{ date: '06-15-25', title: 'workout schedule', slug: 'workout' },
+    //{ date: '06-12-25', title: 'the new new york socialist', slug: 'zohran' },
     { date: '06-10-25', title: 'goals for the summer', slug: 'goals' },
     { date: '06-10-25', title: 'building coherence', slug: 'coherence' },
     { date: '04-03-25', title: 'the stack', slug: 'intro' }
